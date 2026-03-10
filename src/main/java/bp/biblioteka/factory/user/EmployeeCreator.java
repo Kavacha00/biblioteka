c250f121c3755e46a417c28a8ac56bf8e53956fa
Nirrax <nirraxx@gmail.com>
2026-03-10 12:48:31 +0100

    Make EmployeeCreator singleton

--- src/main/java/bp/biblioteka/factory/user/EmployeeCreator.java
+++ src/main/java/bp/biblioteka/factory/user/EmployeeCreator.java
@@ -3,7 +3,27 @@ package bp.biblioteka.factory.user;
 import bp.biblioteka.entity.user.Employee;
 import bp.biblioteka.entity.user.User;
 
-public class EmployeeCreator extends UserCreator{
+public final class EmployeeCreator extends UserCreator{
+    private static volatile EmployeeCreator instance;
+
+    private EmployeeCreator() {
+        super();
+    }
+
+    public static EmployeeCreator getInstance(){
+        EmployeeCreator result = instance;
+        if(result != null){
+            return result;
+        }
+        synchronized(EmployeeCreator.class){
+            if (instance == null) {
+                instance = new EmployeeCreator();
+            }
+            return instance;
+        }
+    }
+
+
     @Override
     public User createUser(String name, String email, String login, String password) {
         return new Employee(name, email, login, password);
