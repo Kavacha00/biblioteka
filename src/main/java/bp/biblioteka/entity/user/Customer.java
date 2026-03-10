f61cd7ea02941976a190fe34c7837bb313632d2a
Nirrax <nirraxx@gmail.com>
2026-03-10 13:31:05 +0100

    Make user entities be a prototype

--- src/main/java/bp/biblioteka/entity/user/Customer.java
+++ src/main/java/bp/biblioteka/entity/user/Customer.java
@@ -1,8 +1,14 @@
 package bp.biblioteka.entity.user;
 
+import java.util.UUID;
+
 public class Customer extends User {
     public Customer(String name, String email, String login, String password) {
         super(name, email, login, password);
+    }
+
+    private Customer(UUID id, String name, String email, String login, String password) {
+        super(id, name, email, login, password);
     }
 
     @Override
@@ -13,5 +19,10 @@ public class Customer extends User {
     @Override
     public String logOut() {
         return "Customer logged out";
+    }
+
+    @Override
+    public User clone() {
+        return new Customer(getId(), getName(), getEmail(), getLogin(), getPassword());
     }
 }
--- src/main/java/bp/biblioteka/entity/user/Employee.java
+++ src/main/java/bp/biblioteka/entity/user/Employee.java
@@ -1,8 +1,14 @@
 package bp.biblioteka.entity.user;
 
+import java.util.UUID;
+
 public class Employee extends User {
     public Employee(String name, String email, String login, String password) {
         super(name, email, login, password);
+    }
+
+    private Employee(UUID id, String name, String email, String login, String password) {
+        super(id, name, email, login, password);
     }
 
     @Override
@@ -14,4 +20,10 @@ public class Employee extends User {
     public String logOut() {
         return "User logged out";
     }
+
+    @Override
+    public User clone() {
+        return new Employee(getId(), getName(), getEmail(), getLogin(), getPassword());
+    }
+
 }
