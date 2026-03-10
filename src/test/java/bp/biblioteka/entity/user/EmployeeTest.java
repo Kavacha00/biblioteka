05b46bfcf0e8335f21f7072ef2249cf44dfb355d
Nirrax <nirraxx@gmail.com>
2026-03-10 13:31:33 +0100

    add test to check if employee fulfills prototype

---
+++ src/test/java/bp/biblioteka/entity/user/EmployeeTest.java
@@ -0,0 +1,25 @@
+package bp.biblioteka.entity.user;
+
+import bp.biblioteka.factory.user.EmployeeCreator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeTest {
+
+    @Test
+    void shouldCloneEmployeeWithCorrectFields(){
+        var factory = EmployeeCreator.getInstance();
+        User user = factory.createUser("John Doe",
+                "John@doe.com", "John's Login", "John's Password");
+        User clone = user.clone();
+
+        assertNotSame(user, clone);
+
+        assertEquals(user.getId(), clone.getId());
+        assertEquals(user.getName(), clone.getName());
+        assertEquals(user.getEmail(), clone.getEmail());
+        assertEquals(user.getLogin(), clone.getLogin());
+        assertEquals(user.getPassword(), clone.getPassword());
+    }
+}
