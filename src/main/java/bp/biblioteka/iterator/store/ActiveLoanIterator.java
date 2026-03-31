f424ab298f72274d1092e7b35d5e974e4cac71c4
Zofia Borys <zosiaborys.zb@gmail.com>
2026-03-31 17:48:53 +0200

    added active loan iterator

---
+++ src/main/java/bp/biblioteka/iterator/store/ActiveLoanIterator.java
@@ -0,0 +1,33 @@
+package bp.biblioteka.iterator.store;
+
+import bp.biblioteka.adapter.store.Loan;
+
+import java.util.List;
+
+public class ActiveLoanIterator implements LoanIterator {
+    private final List<Loan> loans;
+    private int index = 0;
+
+    public ActiveLoanIterator(List<Loan> loans) {
+        this.loans = loans;
+        moveToNextActive();
+    }
+
+    private void moveToNextActive() {
+        while (index < loans.size() && loans.get(index).getEndDate() != null) {
+            index++;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < loans.size();
+    }
+
+    @Override
+    public Loan next() {
+        Loan loan = loans.get(index++);
+        moveToNextActive();
+        return loan;
+    }
+}
