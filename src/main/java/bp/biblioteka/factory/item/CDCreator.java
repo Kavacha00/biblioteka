8d1c33ceaedd68b8d9d76cd97689932a7f451474
DEM0L1SHER <adam.krawczyk2000@gmail.com>
2026-04-21 19:48:11 +0200

    rozbudowa factory i facade o tworzenie plyt CD

--- src/main/java/bp/biblioteka/factory/item/CDCreator.java
+++ src/main/java/bp/biblioteka/factory/item/CDCreator.java
@@ -1,8 +1,19 @@
 package bp.biblioteka.factory.item;
 
-//public class CDCreator extends ItemCreator {
-//    @Override
-//    public Item createItem(String band, String title, ItemFormat format) {
-//        return new CD(band, title,  format);
-//    }
-//}
+import bp.biblioteka.bridge.item.ItemFormat;
+import bp.biblioteka.entity.item.CD;
+import bp.biblioteka.entity.item.Item;
+
+public class CDCreator extends ItemCreator {
+    private static CDCreator instance;
+    private CDCreator() {super();}
+
+    public static CDCreator getInstance() {
+        if (instance == null) instance = new CDCreator();
+        return instance;
+    }
+    @Override
+    public Item createItem(String band, String title, ItemFormat format) {
+        return new CD(band, title,  format);
+    }
+}
