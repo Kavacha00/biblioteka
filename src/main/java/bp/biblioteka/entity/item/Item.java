828b0c269c73344c0e10a9ebb7de9094e123871d
DEM0L1SHER <adam.krawczyk2000@gmail.com>
2026-05-15 12:43:41 +0200

    usuniecie zakomentowanego kodu itp

--- src/main/java/bp/biblioteka/entity/item/Item.java
+++ src/main/java/bp/biblioteka/entity/item/Item.java
@@ -60,14 +60,6 @@ public abstract class Item {
         this.author = memento.getAuthor();
     }
 
-//    public boolean isBorrowed = false;
-//    public boolean isBorrowed() {
-//        return isBorrowed;
-//    }
-//    public void setBorrowed(boolean borrowed) {
-//        this.isBorrowed = borrowed;
-//    }
-
 
     @Override
     public boolean equals(Object o) {
